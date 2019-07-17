e86000fde4b641750664e35bf68e1945456b5ac7
xivk <ben@itinero.tech>
2019-07-17 16:14:14 +0100

    Added skeleton test projects.

---
+++ test/Itinero.MapMatching.Test.Functional/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Itinero.MapMatching.Test.Functional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
