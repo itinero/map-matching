0a5d3cf139c96b6e0cd369571dd673f8ab49325d
xivk <ben@itinero.tech>
2019-08-29 10:42:45 +0100

    Added extension method to make it easier to use the matching.

---
+++ src/Itinero.MapMatching/RouterExtensions.cs
@@ -0,0 +1,23 @@
+using Itinero.Profiles;
+
+namespace Itinero.MapMatching
+{
+    /// <summary>
+    /// Contains extension method for the router class.
+    /// </summary>
+    public static class RouterExtensions
+    {
+        /// <summary>
+        /// Matches the given track using the given profile to the road network.
+        /// </summary>
+        /// <param name="router">The router.</param>
+        /// <param name="profile">The profile to use.</param>
+        /// <param name="track">The track to match.</param>
+        /// <returns></returns>
+        public static MapMatcherResult Match(this Router router, Profile profile, Track track)
+        {
+            var matcher = new MapMatcher(router, profile);
+            return matcher.Match(track);
+        }
+    }
+}
